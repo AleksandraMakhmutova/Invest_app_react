9ffa8a0ef420b363cf9f73329959fecf9c262ffc
AleksandraMakhmutova <honeyms@mail.ru>
2021-03-09 12:37:57 +0300

    for deploy

--- client/src/Component/BackAndHeader/index.js
+++ client/src/Component/BackAndHeader/index.js
@@ -2,18 +2,22 @@ import React from "react";
 import { Button } from "react-scroll";
 import InfoLine from "../InfoLine";
 import style from "./style.module.css";
+import { Link } from "react-router-dom";
 export default function BackAndHeader() {
   return (
     <>
       <InfoLine />
       <div className={style.cardHeader}>
         <div className={style.button}>
-          <div className={style.arrow}>
-            <i class="fas fa-arrow-alt-circle-left"></i>
-          </div>
-          <div>Select your investion frame</div>
+          <Link to="/dashboard">
+            <div className={style.arrow}>
+              <i class="fas fa-arrow-alt-circle-left"></i>
+            </div>
+            <div>Select your investion frame</div>{" "}
+          </Link>
         </div>
         <div className={style.cardBackandHeader}>
+          {" "}
           <h2>What would you do if the market takes a downturn?</h2>
         </div>
       </div>
