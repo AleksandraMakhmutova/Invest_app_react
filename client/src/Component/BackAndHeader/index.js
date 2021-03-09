be32e20819b02e68e6271f2b712259a1931db231
AleksandraMakhmutova <honeyms@mail.ru>
2021-03-09 13:05:59 +0300

    for deploy

--- client/src/Component/BackAndHeader/index.js
+++ client/src/Component/BackAndHeader/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-scroll";
+
 import InfoLine from "../InfoLine";
 import style from "./style.module.css";
 import { Link } from "react-router-dom";
